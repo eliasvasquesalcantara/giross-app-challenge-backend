2d754f4892727de00136f7fad25d0e14cd813247
Elias Vasques <eliasvasques74@gmail.com>
2023-11-22 17:23:18 -0300

    feat: Typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/entities/User';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'dbuser',
+      password: 'dbpassword',
+      database: 'dbpostgres',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
